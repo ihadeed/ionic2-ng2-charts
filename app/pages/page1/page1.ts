8d8904abb5adcb877c1576843be32f0f9f56010f
Ibrahim Hadeed <ibby93@gmail.com>
2016-02-04 20:28:31 -0500

    stuff

--- app/pages/page1/page1.ts
+++ app/pages/page1/page1.ts
@@ -9,4 +9,70 @@ export class Page1 {
   constructor() {
 
   }
+
+
+  // lineChart
+  private lineChartData:Array<any> = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90],
+    [18, 48, 77, 9, 100, 27, 40]
+  ];
+  private lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  private lineChartSeries:Array<any> = ['Series A', 'Series B', 'Series C'];
+  private lineChartOptions:any = {
+    animation: true,
+    responsive: true,
+    multiTooltipTemplate: '<%if (datasetLabel){%><%=datasetLabel %>: <%}%><%= value %>'
+  };
+  private lineChartColours:Array<any> = [
+    { // grey
+      fillColor: 'rgba(148,159,177,0.2)',
+      strokeColor: 'rgba(148,159,177,1)',
+      pointColor: 'rgba(148,159,177,1)',
+      pointStrokeColor: '#fff',
+      pointHighlightFill: '#fff',
+      pointHighlightStroke: 'rgba(148,159,177,0.8)'
+    },
+    { // dark grey
+      fillColor: 'rgba(77,83,96,0.2)',
+      strokeColor: 'rgba(77,83,96,1)',
+      pointColor: 'rgba(77,83,96,1)',
+      pointStrokeColor: '#fff',
+      pointHighlightFill: '#fff',
+      pointHighlightStroke: 'rgba(77,83,96,1)'
+    },
+    { // grey
+      fillColor: 'rgba(148,159,177,0.2)',
+      strokeColor: 'rgba(148,159,177,1)',
+      pointColor: 'rgba(148,159,177,1)',
+      pointStrokeColor: '#fff',
+      pointHighlightFill: '#fff',
+      pointHighlightStroke: 'rgba(148,159,177,0.8)'
+    }
+  ];
+  private lineChartLegend:boolean = true;
+  private lineChartType:string = 'Line';
+
+  private randomize() {
+    let _lineChartData = [];
+    for (let i = 0; i < this.lineChartData.length; i++) {
+      _lineChartData[i] = [];
+      for (let j = 0; j < this.lineChartData[i].length; j++) {
+        _lineChartData[i].push(Math.floor((Math.random() * 100) + 1));
+
+      }
+    }
+    this.lineChartData = _lineChartData;
+  }
+
+  // events
+  chartClicked(e:any) {
+    console.log(e);
+  }
+
+  chartHovered(e:any) {
+    console.log(e);
+  }
+
+
 }
